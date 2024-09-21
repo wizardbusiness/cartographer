172a54fc4182509d78694706c1e4f804fa86bb9d
Gabriel <gkime91@gmail.com>
2024-09-21 16:09:41 -0700

    added writeToJSON util for writing classstring to json for tw jit compiler

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const writeToJson = (classString, relPath) => {
+  const __dirname = path.resolve(path.dirname("./src"));
 
+  const jsonFile = path.join(__dirname, relPath);
+
+  if (!fs.existsSync(jsonFile)) {
+    return;
+  }
+  const fileContent = fs.readFileSync(jsonFile, "utf-8");
+
+  let json = JSON.parse(fileContent);
+  // const key = (classString.match(/\S*:\S*/g) || []).join(" ");
+  const key = classString;
+  if (key in json || key.length === 0) return;
+  setTimeout(() => (json = {}), 1);
+  json[key] = key;
+  const jString = JSON.stringify(json, null, 2);
+  if (process.env.NODE_ENV === "development") {
+    fs.writeFileSync(jsonFile, jString, "utf-8");
+  }
+};
+
+export default writeToJson;
